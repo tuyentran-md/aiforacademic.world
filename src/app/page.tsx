5b4f64bc3e21427c25efcad4ca1d69ef2bf0cdf5
tuyen <tuyen.tran97@gmail.com>
2026-04-20 06:21:53 +0700

    fix: correct navbar logo link and align homepage banners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
               </Link>
             </div>
 
-            <div className="grid gap-4 md:grid-cols-3">
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
               <a
                 href="/ric"
                 target="_blank"
